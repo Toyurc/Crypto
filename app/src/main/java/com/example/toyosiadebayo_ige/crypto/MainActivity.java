4bca464389a65c9af2327fa0762e5f5c773cdfae
Omotoyosi Adebayo-Ige <toyosi.adebayo-ige@konga.com>
2017-10-27 15:23:33 +0100

    initial commit

---
+++ app/src/main/java/com/example/toyosiadebayo_ige/crypto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.toyosiadebayo_ige.crypto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
